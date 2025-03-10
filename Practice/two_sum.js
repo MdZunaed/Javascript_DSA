b9fb12badac387907a0d52f4c8fd06562ca55344
Zunayed <mdzunaed4@gmail.com>
2025-03-11 02:10:20 +0600

    two sum hash map

--- Practice/two_sum.js
+++ Practice/two_sum.js
@@ -13,13 +13,15 @@ Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 */
 
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
 let nums = [2, 11, 8, 7, 12, 18, 15];
 let target = 9
 
 function twoSum(nums, target) {
     for (let i = 0; i < nums.length; i++) {
 
-        for (let j = i+1; j < nums.length; j++) {
+        for (let j = i + 1; j < nums.length; j++) {
             if (nums[i] + nums[j] === target) {
                 return [i, j];
             }
@@ -27,4 +29,20 @@ function twoSum(nums, target) {
     }
 }
 
-console.log(twoSum(nums, target));
+//console.log(twoSum(nums, target));
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+function twoSum2(nums, target) {
+    const map = new Map(); 
+    for (let i = 0; i < nums.length; i++) {
+        let other = target - nums[i];
+        if (map.has(other)) {
+            return [map.get(other), i]; 
+        }
+        map.set(nums[i], i);
+    }
+}
+
+console.log(twoSum2(nums, target));
+
