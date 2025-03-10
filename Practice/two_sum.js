11a2f923be0356bac1851a80931fbc9e5b883532
Zunayed <mdzunaed4@gmail.com>
2025-03-10 22:01:32 +0600

    two sum solved

--- Practice/two_sum.js
+++ Practice/two_sum.js
@@ -4,18 +4,35 @@
 
 // You can return the answer in any order.
 
- 
-
 // Example 1:
-
 // Input: nums = [2,7,11,15], target = 9
 // Output: [0,1]
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-// Example 2:
 
+// Example 2:
 // Input: nums = [3,2,4], target = 6
 // Output: [1,2]
-// Example 3:
 
+// Example 3:
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
+
+let nums = [2, 11, 8, 7, 12, 18, 15];
+let target = 9
+
+function twoSum(nums) {
+    let results = [];
+    for (let i = 0; i < nums.length; i++) {
+
+        for (let j = 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                //console.log(`[${i}, ${j}]`);
+                //return;
+                results = [i, j];
+            }
+        }
+    }
+    return results;
+}
+
+console.log(twoSum(nums, 9));
