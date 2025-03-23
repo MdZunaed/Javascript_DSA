ac24697eccc520034c918a1662ab656cef93effc
Zunayed <mdzunaed4@gmail.com>
2025-03-23 23:58:27 +0600

    comment

--- Practice/valid_bracket.js
+++ Practice/valid_bracket.js
@@ -1,22 +1,22 @@
-function isValid(s) {
-    let stack = [];
-    let pairs = {
-        ")": "(",
-        "}": "{",
-        "]": "[",
-    };
+// function isValid(s) {
+//     let stack = [];
+//     let pairs = {
+//         ")": "(",
+//         "}": "{",
+//         "]": "[",
+//     };
 
-    for (let char of s) {
-        if (char in pairs) {
-            if (stack.length === 0 || stack.pop() != pairs[char]) {
-                return false;
-            }
-        } else {
-            stack.push(char);
-        }
-    }
-    return stack.length === 0;
-}
+//     for (let char of s) {
+//         if (char in pairs) {
+//             if (stack.length === 0 || stack.pop() != pairs[char]) {
+//                 return false;
+//             }
+//         } else {
+//             stack.push(char);
+//         }
+//     }
+//     return stack.length === 0;
+// }
 function isValid(s) {
     let stack = [];
     let pairs = {
