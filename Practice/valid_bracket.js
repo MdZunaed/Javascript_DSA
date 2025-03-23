060f10e8cfbaea5e127015965056ab55c946c405
Zunayed <mdzunaed4@gmail.com>
2025-03-24 02:55:13 +0600

    valid bracket alternative

--- Practice/valid_bracket.js
+++ Practice/valid_bracket.js
@@ -1,22 +1,3 @@
-// function isValid(s) {
-//     let stack = [];
-//     let pairs = {
-//         ")": "(",
-//         "}": "{",
-//         "]": "[",
-//     };
-
-//     for (let char of s) {
-//         if (char in pairs) {
-//             if (stack.length === 0 || stack.pop() != pairs[char]) {
-//                 return false;
-//             }
-//         } else {
-//             stack.push(char);
-//         }
-//     }
-//     return stack.length === 0;
-// }
 function isValid(s) {
     let stack = [];
     let pairs = {
@@ -40,7 +21,7 @@ function isValid(s) {
 let test1 = "()";
 let test2 = "(]";
 let test3 = "[()]";
-let test4 = "(){}[[";
+let test4 = "()[[{}";
 let test5 = "(){}[]";
 
 console.log(test1 + ' valid: ' + isValid(test1));
@@ -48,3 +29,25 @@ console.log(test2 + ' valid: ' + isValid(test2));
 console.log(test3 + ' valid: ' + isValid(test3));
 console.log(test4 + ' valid: ' + isValid(test4));
 console.log(test5 + ' valid: ' + isValid(test5));
+
+
+/// Alternative
+
+// function isValid(s) {
+//     let stack = [];
+//     let map = {
+//         ')': '(',
+//         '}': '{',
+//         ']': '[',
+//     };
+//     for (let i = 0; i < s.length; i++) {
+//         let current = s[i];
+//         if (current in map) {
+//             if (map[current] != stack.pop()) return false;
+//         } else {
+//             stack.push(current);
+//         }
+//     }
+
+//     return stack.length == 0;
+// }
